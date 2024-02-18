ea181211a9ef8ac01a4e320162a1c2c0dea71b3a
Mikhail Vasiliev <m89154280096@yandex.ru>
2024-02-18 19:43:25 +0300

    Added conftest, id replaced with #

--- test_fill_form.py
+++ test_fill_form.py
@@ -1,26 +1,24 @@
 from selene import browser, be, have, command
-import pytest
 import os
 
 
 def test_filling_table():
-    browser.open('https://demoqa.com/automation-practice-form')
-
+    browser.open('/')
     # filling form
-    browser.element('[id="firstName"]').should(be.blank).type('Yashaka')
-    browser.element('[id="lastName"]').should(be.blank).type('Selenium')
-    browser.element('[id="userEmail"]').should(be.blank).type('Examplum@mulpmaxe.com')
+    browser.element('#firstName').should(be.blank).type('Yashaka')
+    browser.element('#lastName').should(be.blank).type('Selenium')
+    browser.element('#userEmail').should(be.blank).type('Examplum@mulpmaxe.com')
     browser.all('[name=gender]').element_by(have.value_containing('Male')).with_(click_by_js=True).click()
-    browser.element('[id="userNumber"]').should(be.blank).type('8005553535')
-    browser.element('[id="dateOfBirthInput"]').perform(command.select_all).type('19 Nov 1900').press_enter()
-    browser.element('[id="subjectsInput"]').type('Math').press_enter()
-    browser.element('[id="hobbies-checkbox-2"]').with_(click_by_js=True).click()
-    browser.element('[id="hobbies-checkbox-3"]').with_(click_by_js=True).click()
-    browser.element('[id="uploadPicture"]').send_keys(os.path.abspath('pic.png'))
-    browser.element('[id="currentAddress"]').should(be.blank).type('Colorado-Springs')
-    browser.element('[id="state"]').click().element('[id="react-select-3-option-0"]').click()
-    browser.element('[id="city"]').click().element('[id="react-select-4-option-2"]').click()
-    browser.element('[id="submit"]').click()
+    browser.element('#userNumber').should(be.blank).type('8005553535')
+    browser.element('#dateOfBirthInput').perform(command.select_all).type('19 Nov 1900').press_enter()
+    browser.element('#subjectsInput').type('Math').press_enter()
+    browser.element('#hobbies-checkbox-2').with_(click_by_js=True).click()
+    browser.element('#hobbies-checkbox-3').with_(click_by_js=True).click()
+    browser.element('#uploadPicture').send_keys(os.path.abspath('pic.png'))
+    browser.element('#currentAddress').should(be.blank).type('Colorado-Springs')
+    browser.element('#state').click().element('#react-select-3-option-0').click()
+    browser.element('#city').click().element('#react-select-4-option-2').click()
+    browser.element('#submit').click()
 
     # check for successfully filled tab
     browser.element('.modal-header').should(have.text('Thanks for submitting the form'))
@@ -38,5 +36,4 @@ def test_filling_table():
             'Picture', 'pic.png',
             'Address', 'Colorado-Springs',
             'State and City', 'NCR Noida'))
-    browser.element('[id="closeLargeModal"]').click()
-    browser.quit()
+    browser.element('#closeLargeModal').click()
